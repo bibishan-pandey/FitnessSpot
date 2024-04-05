7ed43099fa7a6b005ff559d7e9b41fcf28f1c7da
Buddha Gubhaju <buddhagubhaju5@gmail.com>
2024-04-04 21:39:12 -0400

    Add post model and its migration

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,2 +1,3 @@
+from .post import Post
 from .workout import Workout
 from .workout_type import WorkoutType
---
+++ core/models/post.py
@@ -0,0 +1,35 @@
+import shortuuid
+from django.db import models
+from django.utils.text import slugify
+from shortuuid.django_fields import ShortUUIDField
+
+from auths.models import User
+from core.models.base import BaseModel
+from core.models.workout import Workout
+from utils.choices import VISIBILITY
+from utils.media_upload import upload_post_image_location, upload_post_video_location
+
+
+class Post(BaseModel):
+    pid = ShortUUIDField(length=8, max_length=26, alphabet='abcdefghijklmnopqrstuvqxyz', unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to=upload_post_image_location, blank=True, null=True)
+    video = models.FileField(upload_to=upload_post_video_location, blank=True, null=True)
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE, null=True, blank=True)
+    slug = models.SlugField(max_length=255, unique=True, null=True, blank=True)
+    visibility = models.CharField(max_length=15, choices=VISIBILITY, default='public')
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+    active = models.BooleanField(default=True)
+    views = models.PositiveIntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        if self.slug is None or self.slug == "":
+            uuid_key = shortuuid.uuid()
+            self.slug = str(uuid_key.lower()) + "-" + slugify(self.content[:10] if self.content else "")
+        super(Post, self).save(*args, **kwargs)
+
+    def __str__(self):
+        if self.content:
+            return self.content
+        return self.author.username
