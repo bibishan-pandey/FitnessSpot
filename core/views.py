db336234fd7789ad28c4b5e206c4adbcde1deab9
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2024-04-06 22:04:57 -0400

    List all posts with workout details and filter by visibility

--- core/views.py
+++ core/views.py
@@ -1,7 +1,19 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render
+
+from core.models import Post
 
 
 @login_required
 def index(request):
-    return render(request, 'core/index.html')
+    # TODO: Fix for friends posts
+    posts = (Post.objects
+             .filter(Q(visibility="public") | Q(author__friends__user=request.user), active=True)
+             .order_by('-created_at'))
+
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'core/index.html', context)
