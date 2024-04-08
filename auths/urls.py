4e1a40ffb6193610e1988e55026bc97073a98fa1
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2024-04-08 13:13:05 -0400

    Add user profile and my profile

--- auths/urls.py
+++ auths/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('register/', views.register, name='fitness-register'),
     path('login/', views.signin, name='fitness-login'),
     path('logout/', views.signout, name='fitness-logout'),
+    path('my-profile/', views.my_profile, name='fitness-profile'),
+    path('profile/<str:username>/', views.others_profile, name='fitness-others-profile'),
 ]
--- auths/views.py
+++ auths/views.py
@@ -1,9 +1,13 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render, redirect
 
 from auths.forms import UserCreationForm
-from auths.models import Profile
+from auths.models import Profile, User
+from core.models import Workout, Friend, FriendRequest
+from utils.posts import get_posts_with_comments
 
 
 def register(request, *args, **kwargs):
@@ -66,3 +70,82 @@ def signin(request, *args, **kwargs):
 def signout(request):
     logout(request)
     return redirect("auths:fitness-register")
+
+
+@login_required
+def my_profile(request):
+    # Get the IDs of friends for the current user
+    user_friends_ids = Friend.objects.filter(user=request.user).values_list('friend_id', flat=True)
+
+    # Get the IDs of users who have the current user as their friend
+    user_users_ids = Friend.objects.filter(friend=request.user).values_list('user_id', flat=True)
+
+    # Combine the two lists to get all friends both ways
+    friends_ids = set(user_friends_ids) | set(user_users_ids)
+
+    # Now you have the IDs of all friends, you can query the User model or
+    # any other related model to get more information about these friends if needed
+    friends = User.objects.filter(id__in=friends_ids)
+
+    posts = get_posts_with_comments(request, author=request.user)
+    workouts = Workout.objects.filter(author=request.user)
+
+    context = {
+        'current_user': request.user,
+        'profile': request.user.profile,
+        'friends': friends,
+        'posts': posts,
+        'workouts': workouts
+    }
+    return render(request, 'auths/my-profile.html', context)
+
+
+@login_required
+def others_profile(request, username):
+    other_user = User.objects.get(username=username)
+
+    # Get the IDs of friends for the current user
+    user_friends_ids = Friend.objects.filter(user=other_user).values_list('friend_id', flat=True)
+
+    # Get the IDs of users who have the current user as their friend
+    user_users_ids = Friend.objects.filter(user=other_user).values_list('user_id', flat=True)
+
+    # Combine the two lists to get all friends both ways
+    friends_ids = set(user_friends_ids) | set(user_users_ids)
+
+    # Now you have the IDs of all friends, you can query the User model or
+    # any other related model to get more information about these friends if needed
+    friends = User.objects.filter(id__in=friends_ids)
+
+    posts = get_posts_with_comments(request, author=other_user)
+    workouts = Workout.objects.filter(author=other_user)
+
+    is_friend_request_sent = False
+
+    from_user = request.user
+    to_user = other_user
+
+    # Lookup friend request
+    try:
+        friend_request = FriendRequest.objects.get(
+            Q(from_user=from_user, to_user=to_user) | Q(from_user=to_user, to_user=from_user))
+        if friend_request.status == 'pending':
+            is_friend_request_sent = True
+
+    except FriendRequest.DoesNotExist:
+        is_friend_request_sent = False
+
+    # Check for friendship
+    is_friend = Friend.objects.filter(Q(user=from_user, friend=to_user) | Q(friend=from_user, user=to_user)).exists()
+
+    context = {
+        'current_user': request.user,
+        'other_user': other_user,
+        'profile': other_user.profile,
+        'friends': friends,
+        'posts': posts,
+        'workouts': workouts,
+        'is_friend_request_sent': is_friend_request_sent,
+        'is_friend': is_friend
+    }
+    return render(request, 'auths/user-profile.html', context)
